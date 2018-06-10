3fcfcb0afe5a3414754da1b007f0f391bcb47ee9
Matt McAlister <matt.mcalister93@gmail.com>
2018-06-10 14:07:16 -0400

    implemented context api to access UserAuth throughout app

--- src/components/App.js
+++ src/components/App.js
@@ -14,61 +14,42 @@ import LandingPage from './Landing';
 import AccountPage from './Account';
 
 import * as routes from '../constants/routes';
-import { firebase } from '../firebase';
+import withAuthentication from './withAuthentication';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  return (
+  <Router>
+    <div>
+      <Navigation  />
 
-    this.state = {
-      authUser: null,
-    };
-  }
+      <hr/>
 
-  componentDidMount() {
-    firebase.auth.onAuthStateChanged(authUser => {
-      authUser
-        ? this.setState(() => ({ authUser }))
-        : this.setState(() => ({ authUser: null }));
-    });
-  }
-
-
-  render() {
-    return (
-    <Router>
-      <div>
-        <Navigation authUser={this.state.authUser} />
-
-        <hr/>
-
-        <Route
-          exact path={routes.LANDING}
-          component={() => <LandingPage />}
-        />
-        <Route
-          exact path={routes.SIGN_UP}
-          component={() => <SignUpPage />}
-        />
-        <Route
-          exact path={routes.SIGN_IN}
-          component={() => <SignInPage />}
-        />
-        <Route
-          exact path={routes.PASSWORD_FORGET}
-          component={() => <PasswordForgetPage />}
-        />
-        <Route
-          exact path={routes.HOME}
-          component={() => <HomePage />}
-        />
-        <Route
-          exact path={routes.ACCOUNT}
-          component={() => <AccountPage />}
-        />
-      </div>
-    </Router>
-    )
-  }
+      <Route
+        exact path={routes.LANDING}
+        component={() => <LandingPage />}
+      />
+      <Route
+        exact path={routes.SIGN_UP}
+        component={() => <SignUpPage />}
+      />
+      <Route
+        exact path={routes.SIGN_IN}
+        component={() => <SignInPage />}
+      />
+      <Route
+        exact path={routes.PASSWORD_FORGET}
+        component={() => <PasswordForgetPage />}
+      />
+      <Route
+        exact path={routes.HOME}
+        component={() => <HomePage />}
+      />
+      <Route
+        exact path={routes.ACCOUNT}
+        component={() => <AccountPage />}
+      />
+    </div>
+  </Router>
+  )
 }
-export default App;
+export default withAuthentication(App);
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,29 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import AuthUserContext from './AuthUserContext';
 import SignOutButton from './sessions/SignOutButton';
 import * as routes from '../constants/routes';
 
-const Navigation = ({ authUser }) =>
-  <div>
-    { authUser
-        ? <NavigationAuth />
-        : <NavigationNonAuth />
+const Navigation = () => {
+  return (<AuthUserContext.Consumer>
+    {authUser => authUser
+      ? <NavigationAuth />
+      : <NavigationNonAuth />
     }
-  </div>
+  </AuthUserContext.Consumer>)
+}
 
-const NavigationAuth = () =>
-  <ul>
+const NavigationAuth = () => {
+  return (<ul>
     <li><Link to={routes.LANDING}>Landing</Link></li>
     <li><Link to={routes.HOME}>Home</Link></li>
     <li><Link to={routes.ACCOUNT}>Account</Link></li>
     <li><SignOutButton /></li>
-  </ul>
-
-const NavigationNonAuth = () =>
-  <ul>
+  </ul>)
+}
+const NavigationNonAuth = () => {
+  return (<ul>
     <li><Link to={routes.LANDING}>Landing</Link></li>
     <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
-  </ul>
-
+  </ul>)
+}
 export default Navigation;
